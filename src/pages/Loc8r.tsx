f461a2d4a5f3923952b428f3810df6b862230465
[dyad] <git@dyad.sh>
2025-10-19 12:56:04 +0530

    [dyad] wrote 1 file(s)

--- src/pages/Loc8r.tsx
+++ src/pages/Loc8r.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import AddPlaceForm from '@/components/AddPlaceForm';
 
 interface Place {
   _id: string;
